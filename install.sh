68ee501782f92d615ae34f56f010c11a759ae320
mike90s15 <82988362+mike90s15@users.noreply.github.com>
2023-02-15 12:21:21 +0000

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+printf " O programa está em desenvolvimento e pode apresata bugs\n"
+sleep 1
+pkg="sudo apt-get"
+if [[ -e /data/data/com.termux ]]; then
+  pkg="pkg"
+else
+  [[ $UID != 0 ]] && echo " Por favor excute com usuario root" && exit
+fi
+if [[ -z "$(command -v python)" ]]; then
+  printf " Instalando o python...\n"
+  $pkg install -y python &>/dev/null
+fi
+printf " Instalndo os requimentos para o programa...\n"
+pip install -r requirements.txt &>/dev/null
+python main.py

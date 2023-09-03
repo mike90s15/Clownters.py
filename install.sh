2b278200ecea0ff41c39dca2cc19f14767661a3b
mike90s15 <82988362+mike90s15@users.noreply.github.com>
2023-09-03 15:53:01 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,17 @@
 #!/usr/bin/env bash
-xdg-open https://instagram.com/mike90s15 &>/dev/null && sleep 30
-xdg-open https://twitter.com/mike90s15 &>/dev/null && sleep 30
-xdg-open https://www.tiktok.com/@mike90s15 &>/dev/null && sleep 30
-xdg-open https://t.me/channel_90s15 &>/dev/null && sleep 30
+xdg-open https://t.me/channel_90s15
+printf " O programa está em desenvolvimento e pode apresata bugs\n"
+sleep 1
+pkg="sudo apt-get"
+if [[ -e /data/data/com.termux ]]; then
+  pkg="pkg"
+else
+  [[ $UID != 0 ]] && echo " Por favor excute com usuario root" && exit
+fi
+if [[ -z "$(command -v python)" ]]; then
+  printf " Instalando o python...\n"
+  $pkg install -y python &>/dev/null
+fi
+printf " Instalndo os requimentos para o programa...\n"
+pip install -r requirements.txt &>/dev/null
+python main.py
